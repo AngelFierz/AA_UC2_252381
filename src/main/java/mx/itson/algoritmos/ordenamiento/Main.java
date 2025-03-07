49898421840b3c77c5924d652705c29a8840f097
Propietario <Propietario@host.docker.internal>
2025-03-07 16:30:50 -0700

    Modifaciones grandes en Main.

--- src/main/java/mx/itson/algoritmos/ordenamiento/Main.java
+++ src/main/java/mx/itson/algoritmos/ordenamiento/Main.java
@@ -9,15 +9,18 @@ package mx.itson.algoritmos.ordenamiento;
  * @author Propietario
  */
 
-public class Main {
+public class Main { // Declaración de independencia de la clase mamaimaiamin
     
-    public static void main(String[] args) {
+    public static void main(String[] args) { // Metodo principal, creo que era entry point
         
-        int[] numeros = {7, 10, 24, 9, 3, 14, 6, 4, 2};
-        Ordenamientos.ordenarBurbuja(numeros);
-        for (int numero : numeros){
-            System.out.print(numero + ", "); // Numeros separados con coma.
+        int[] numeros = {7, 10, 24, 9, 3, 14, 6, 4, 2}; // Arreglos, son 9
+        
+        Ordenamientos.ordenarBurbuja(numeros); // de ordenamientos. ordenarburbuja a numeros
+        
+        for (int numero : numeros) { 
+            System.out.print(numero + ", "); // Imprime la lista de arriba pero con una coma.
         }
+        
     }
 }
 
